b28a8ac26d6733c7d67a6b04f0d0cbc3606f05c2
Connor <connormccaskie@gmail.com>
2021-07-01 14:02:08 +1000

    Continued work on objectives

---
+++ SplitAeon/Assets/_SplitAeon/_Scripts/ObjectiveList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ObjectiveList : MonoBehaviour
+{
+    public string[] objectives;
+
+    private int currentObjective = -1;
+
+    private TextMeshProUGUI text;
+
+    private void Start()
+    {
+        text = gameObject.transform.Find("Objective").Find("Objective text").gameObject.GetComponent<TextMeshProUGUI>();
+        text.text = "";
+    }
+
+    public void NextObjective()
+    {
+        if (currentObjective < objectives.Length - 1)
+        {
+            currentObjective++;
+            text.text = objectives[currentObjective];
+        }
+        else
+        {
+            //Win Game!
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NextObjective();
+        }
+    }
+}
