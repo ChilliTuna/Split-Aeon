82653cacb4be4d1eb8fd3b2e88c0f7d5bc76c245
Connor <connormccaskie@gmail.com>
2021-07-01 16:58:18 +1000

    Continued work on objectives

--- SplitAeon/Assets/_SplitAeon/_Scripts/ObjectiveList.cs
+++ SplitAeon/Assets/_SplitAeon/_Scripts/ObjectiveList.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class ObjectiveList : MonoBehaviour
@@ -11,18 +12,26 @@ public class ObjectiveList : MonoBehaviour
 
     private TextMeshProUGUI text;
 
+    private bool isVisisble = true;
+
     private void Start()
     {
         text = gameObject.transform.Find("Objective").Find("Objective text").gameObject.GetComponent<TextMeshProUGUI>();
         text.text = "";
+        ToggleObjectives(false);
     }
 
     public void NextObjective()
     {
+        currentObjective++;
+        text.text = objectives[currentObjective];
+    }
+
+    public void CompleteObjective()
+    {
         if (currentObjective < objectives.Length - 1)
         {
-            currentObjective++;
-            text.text = objectives[currentObjective];
+            NextObjective();
         }
         else
         {
@@ -30,11 +39,23 @@ public class ObjectiveList : MonoBehaviour
         }
     }
 
+    public void ToggleObjectives(bool state)
+    {
+        isVisisble = state;
+        GetComponent<Image>().enabled = state;
+        transform.Find("Header text").gameObject.SetActive(state);
+        transform.Find("Objective").gameObject.SetActive(state);
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
-            NextObjective();
+            ToggleObjectives(!isVisisble);
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            CompleteObjective();
         }
     }
 }
