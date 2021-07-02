b162950938a5b2bf5011073113d4f028d568f210
Connor <connormccaskie@gmail.com>
2021-07-02 16:11:10 +1000

    Got UI fading

--- SplitAeon/Assets/_SplitAeon/_Scripts/ObjectiveList.cs
+++ SplitAeon/Assets/_SplitAeon/_Scripts/ObjectiveList.cs
@@ -1,37 +1,49 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class ObjectiveList : MonoBehaviour
 {
     public string[] objectives;
 
+    [Header("Transition Times")]
+    public float initialTime = 0.2f;
+
+    public float stage1Time = 0.2f;
+    public float stage2Time = 0.2f;
+    public float stage3Time = 0.2f;
+
+    [Space]
+    public float fadeSpeed = 0.1f;
+
     private int currentObjective = -1;
 
-    private TextMeshProUGUI text;
+    private TextMeshProUGUI objectiveText;
 
-    private bool isVisisble = true;
+    private bool isVisisble = false;
 
     private void Start()
     {
-        text = gameObject.transform.Find("Objective").Find("Objective text").gameObject.GetComponent<TextMeshProUGUI>();
-        text.text = "";
-        ToggleObjectives(false);
+        objectiveText = gameObject.transform.Find("Objective").Find("Objective text").gameObject.GetComponent<TextMeshProUGUI>();
+        objectiveText.text = "";
     }
 
     public void NextObjective()
     {
         currentObjective++;
-        text.text = objectives[currentObjective];
+        objectiveText.text = objectives[currentObjective];
     }
 
     public void CompleteObjective()
     {
         if (currentObjective < objectives.Length - 1)
         {
-            NextObjective();
+            if (!isVisisble)
+            {
+                ToggleObjectives(!isVisisble);
+            }
+            StartCoroutine("TransitionObjective");
         }
         else
         {
@@ -42,9 +54,14 @@ public class ObjectiveList : MonoBehaviour
     public void ToggleObjectives(bool state)
     {
         isVisisble = state;
-        GetComponent<Image>().enabled = state;
-        transform.Find("Header text").gameObject.SetActive(state);
-        transform.Find("Objective").gameObject.SetActive(state);
+        if (!isVisisble)
+        {
+            StartCoroutine(FadeOut(GetComponent<CanvasGroup>(), fadeSpeed));
+        }
+        else
+        {
+            StartCoroutine(FadeIn(GetComponent<CanvasGroup>(), fadeSpeed));
+        }
     }
 
     private void Update()
@@ -56,6 +73,97 @@ public class ObjectiveList : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P))
         {
             CompleteObjective();
+        }
+    }
+
+    private IEnumerator TransitionObjective()
+    {
+        int i = 0;
+        while (i < 5)
+        {
+            switch (i)
+            {
+                case (0):
+                    i++;
+                    yield return new WaitForSeconds(initialTime);
+                    break;
+
+                case (1):
+                    gameObject.transform.Find("Objective").Find("Complete image").gameObject.SetActive(true);
+                    objectiveText.fontStyle = FontStyles.Strikethrough;
+                    i++;
+                    yield return new WaitForSeconds(stage1Time);
+                    break;
+
+                default:
+                    break;
+            }
+            i++;
+            yield return null;
+        }
+        NextObjective();
+    }
+
+    private bool IsApproximatelyEqual(float targetVal, float actualVal, float acceptableVariance = 0.1f)
+    {
+        return (targetVal - acceptableVariance <= actualVal && actualVal + acceptableVariance >= actualVal);
+    }
+
+    private IEnumerator FadeOut(TextMeshProUGUI text, float rate = 1)
+    {
+        while (text.alpha > 0)
+        {
+            text.alpha -= rate * Time.deltaTime;
+            yield return 0;
+        }
+    }
+
+    private IEnumerator FadeOut(Image image, float rate = 1)
+    {
+        while (image.color.a > 0)
+        {
+            Color tempCol = image.color;
+            tempCol.a -= rate * Time.deltaTime;
+            image.color = tempCol;
+            yield return 0;
+        }
+    }
+
+    private IEnumerator FadeOut(CanvasGroup canvasGroup, float rate = 1)
+    {
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= rate * Time.deltaTime;
+            yield return 0;
+        }
+    }
+
+    private IEnumerator FadeIn(TextMeshProUGUI text, float rate = 1)
+    {
+        while (text.alpha < 1)
+        {
+            text.alpha += rate * Time.deltaTime;
+            yield return 0;
+        }
+    }
+
+    private IEnumerator FadeIn(Image image, float rate = 1)
+    {
+        while (image.color.a < 1)
+        {
+            Color tempCol = image.color;
+            tempCol.a += rate * Time.deltaTime;
+            image.color = tempCol;
+            yield return 0;
+        }
+    }
+
+    private IEnumerator FadeIn(CanvasGroup canvasGroup, float rate = 1)
+    {
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += rate * Time.deltaTime;
+            yield return 0;
         }
     }
 }
