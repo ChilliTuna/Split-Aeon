5965f806fdc4e74f1fbaeb7af6d53ab34f257997
Connor <connormccaskie@gmail.com>
2021-07-28 17:35:57 +1000

    Established some deliverables for game manager

--- SplitAeon/Assets/_SplitAeon/_Scripts/Misc/GameManager.cs
+++ SplitAeon/Assets/_SplitAeon/_Scripts/Misc/GameManager.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    //Things this script needs to deliver
+    /*
+        -When should spawners start. This is most likely going to be a trigger box.
+        -When should spawners stop. This "might" be a kill limit. or another trigger box.
+        -When the player has switched zones. Call the function that will "pause" the AI manager for that zone
+        -How are the enemies going to hit the player. Invoking a damage event currently, with a collider attached to their arm. It works horribly right now.
+    */
+
+    public bool ShouldEnemiesSpawn;
 
     // Start is called before the first frame update
     void Start()
