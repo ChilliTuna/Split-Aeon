26a1b37beca0504eb0f63b6f6040d4391f87dff7
Connor <connormccaskie@gmail.com>
2021-07-28 16:56:55 +1000

    Removing duplicate HUD

--- SplitAeon/Assets/_SplitAeon/_Scripts/Misc/GameManager.cs
+++ SplitAeon/Assets/_SplitAeon/_Scripts/Misc/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void WinGame()
+    {
+        //Do stuff (TBD)
     }
 }
