c13567f0771e8db5bc258381150d15c9edfad4d9
Lucas Petersen <ima_lucas@hotmail.com>
2021-08-04 20:31:42 +1000

    Spawner Has a Box to automatically find spawn locations
    Enemy projectile is destryed after some time

--- SplitAeon/Assets/_SplitAeon/_Scripts/AI/Projectile.cs
+++ SplitAeon/Assets/_SplitAeon/_Scripts/AI/Projectile.cs
@@ -6,6 +6,9 @@ public class Projectile : MonoBehaviour
 {
     public float speed = 5.0f;
 
+    public float lifeTime = 5.0f;
+    float m_timer = 0.0f;
+
     Rigidbody m_body;
 
     // Start is called before the first frame update
@@ -13,12 +16,19 @@ public class Projectile : MonoBehaviour
     {
         m_body = GetComponent<Rigidbody>();
         m_body.velocity = transform.forward * speed;
+        m_timer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        m_timer += Time.deltaTime;
         //transform.position += transform.forward * speed * Time.deltaTime;
+
+        if(m_timer > lifeTime)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
