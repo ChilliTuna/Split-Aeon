85435f2f8b397231ce3fd25f7c46eb3302a1b379
Tom <karzacinc@gmail.com>
2021-08-18 16:10:54 +1000

    GUI Changes & Card Manager Added

--- SplitAeon/Assets/CardManager.cs
+++ SplitAeon/Assets/CardManager.cs
@@ -18,12 +18,38 @@ public class CardManager : MonoBehaviour
     public int maxCardLethals;
     public Transform lethalSpawnLocation;
 
+    public Image cardImage;
+
+    private GameObject selectedCard;
+
+    private enum CardTypes
+    {
+        Regular,
+        Warping,
+        Piercing,
+        Splash
+    }
+
+    CardTypes cardTypes;
+
     [Header("Weapon GUI")]
     public TextMeshProUGUI cardPoolReadout;
 
     [Header("Regular Card")]
-    public float cardLethalDamage;
-    public GameObject cardLethalPrefab;
+    public GameObject regularCardPrefab;
+    public Sprite regularCardSprite;
+
+    [Header("Warping Card")]
+    public GameObject warpingCardPrefab;
+    public Sprite warpingCardSprite;
+
+    [Header("Piercing Card")]
+    public GameObject piercingCardPrefab;
+    public Sprite piercingCardSprite;
+
+    [Header("Splash Card")]
+    public GameObject splashCardPrefab;
+    public Sprite splashCardSprite;
 
     [Header("Audio")]
     public AudioSource weaponAudioSource;
@@ -51,6 +77,53 @@ public class CardManager : MonoBehaviour
             }
         }
 
+        switch (cardTypes)
+        {
+            case CardTypes.Regular:
+                selectedCard = regularCardPrefab;
+                cardImage.sprite = regularCardSprite;
+                break;
+
+            case CardTypes.Warping:
+                selectedCard = warpingCardPrefab;
+                cardImage.sprite = warpingCardSprite;
+                break;
+
+            case CardTypes.Piercing:
+                selectedCard = piercingCardPrefab;
+                cardImage.sprite = piercingCardSprite;
+                break;
+
+            case CardTypes.Splash:
+                selectedCard = splashCardPrefab;
+                cardImage.sprite = splashCardSprite;
+                break;
+        }
+
+    }
+
+    public void SetCardType(int index)
+    {
+        if (index == 0)
+        {
+            cardTypes = CardTypes.Regular;
+        }
+        else if (index == 1)
+        {
+            cardTypes = CardTypes.Warping;
+        }
+        else if (index == 2)
+        {
+            cardTypes = CardTypes.Piercing;
+        }
+        else if (index == 3)
+        {
+            cardTypes = CardTypes.Splash;
+        }
+        else
+        {
+            cardTypes = CardTypes.Regular;
+        }
     }
 
     public void ThrowCardLethal()
@@ -62,7 +135,7 @@ public class CardManager : MonoBehaviour
             weaponAudioSource.PlayOneShot(lethalThrowClips[Mathf.FloorToInt(Random.Range(0, lethalThrowClips.Length))]);
 
             GameObject thrownLethal;
-            thrownLethal = Instantiate(cardLethalPrefab, lethalSpawnLocation.transform.position, Quaternion.identity);
+            thrownLethal = Instantiate(selectedCard, lethalSpawnLocation.transform.position, Quaternion.identity);
 
             thrownLethal.GetComponent<Rigidbody>().velocity = lethalSpawnLocation.TransformDirection(0, 3, 20);
             thrownLethal.GetComponent<Rigidbody>().AddRelativeTorque(0, 90, 0);
