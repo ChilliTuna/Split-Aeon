7c45826383ba0277b0ab239851b70b2cb8e6b1f2
Lucas Petersen <ima_lucas@hotmail.com>
2021-09-02 15:04:50 +1000

    Fixed modulo Error with spawnPartitions
    negative numbers were not being considered properly when using modulo. This lead to an off by one error at the origin

--- SplitAeon/Assets/_SplitAeon/_Scripts/AI/Managers/SpawnManager.cs
+++ SplitAeon/Assets/_SplitAeon/_Scripts/AI/Managers/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public Vector3 cellSize = Vector3.one;
+    public Vector3 offset = Vector3.one * 0.5f;
     [Min(1)]public int cellRange = 1;
 
     public Transform playerTransform;
@@ -13,7 +14,7 @@ public class SpawnManager : MonoBehaviour
 
     private void Awake()
     {
-        m_spawnPartitions = new SpawnPartitions(cellSize);
+        m_spawnPartitions = new SpawnPartitions(cellSize, offset);
 
         var spawnerArray = FindObjectsOfType<EnemySpawner>();
 
@@ -52,47 +53,23 @@ public class SpawnManager : MonoBehaviour
 
     public IVec3 GetPlayerCell()
     {
-        return m_spawnPartitions.GetCell(playerTransform.position);
+        return GetPlayerCell(m_spawnPartitions, playerTransform);
+    }
+
+    public static IVec3 GetPlayerCell(SpawnPartitions spawnPartitions, Transform playerTransform)
+    {
+        return spawnPartitions.GetCell(playerTransform.position);
     }
 
     public IVec3[,,] GetPlayerAdjacentCells()
     {
-        IVec3 playerCell = GetPlayerCell();
-        int arraySize = 1 + (cellRange * 2);
-        IVec3[,,] playerCellArray = new IVec3[arraySize, arraySize, arraySize];
-
-        for(int x = 0; x < arraySize; x++)
-        {
-            for (int y = 0; y < arraySize; y++)
-            {
-                for (int z = 0; z < arraySize; z++)
-                {
-                    IVec3 adjacentCell = IVec3.zero;
-                    adjacentCell.x = playerCell.x + x - cellRange;
-                    adjacentCell.y = playerCell.y + y - cellRange;
-                    adjacentCell.z = playerCell.z + z - cellRange;
-
-                    playerCellArray[x, y, z] = adjacentCell;
-                }
-            }
-        }
-
-        return playerCellArray;
+        return GetPlayerAdjacentCells(this, m_spawnPartitions, playerTransform);
     }
 
-    private void OnDrawGizmosSelected()
+    public static IVec3[,,] GetPlayerAdjacentCells(SpawnManager spawnManager, SpawnPartitions spawnPartitions, Transform playerTransform)
     {
-        Gizmos.color = Color.cyan;
-
-        SpawnPartitions spawnPartitions = m_spawnPartitions;
-        if (spawnPartitions == null)
-        {
-            spawnPartitions = new SpawnPartitions(cellSize);
-        }
-
-        IVec3 playerCell = spawnPartitions.GetCell(playerTransform.position);
-
-        int arraySize = 1 + (cellRange * 2);
+        IVec3 playerCell = GetPlayerCell(spawnPartitions, playerTransform);
+        int arraySize = 1 + (spawnManager.cellRange * 2);
         IVec3[,,] playerCellArray = new IVec3[arraySize, arraySize, arraySize];
 
         for (int x = 0; x < arraySize; x++)
@@ -102,22 +79,62 @@ public class SpawnManager : MonoBehaviour
                 for (int z = 0; z < arraySize; z++)
                 {
                     IVec3 adjacentCell = IVec3.zero;
-                    adjacentCell.x = playerCell.x + x - cellRange;
-                    adjacentCell.y = playerCell.y + y - cellRange;
-                    adjacentCell.z = playerCell.z + z - cellRange;
+                    adjacentCell.x = playerCell.x + x - spawnManager.cellRange;
+                    adjacentCell.y = playerCell.y + y - spawnManager.cellRange;
+                    adjacentCell.z = playerCell.z + z - spawnManager.cellRange;
 
                     playerCellArray[x, y, z] = adjacentCell;
                 }
             }
         }
 
+        return playerCellArray;
+    }
+
+    private void OnValidate()
+    {
+        offset.x = Mathf.Clamp(offset.x, -1.0f, 1.0f);
+        offset.y = Mathf.Clamp(offset.y, -1.0f, 1.0f);
+        offset.z = Mathf.Clamp(offset.z, -1.0f, 1.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+
+        SpawnPartitions spawnPartitions = m_spawnPartitions;
+        if (spawnPartitions == null)
+        {
+            spawnPartitions = new SpawnPartitions(cellSize, offset);
+        }
+
+        IVec3[,,] playerCellArray = GetPlayerAdjacentCells(this, spawnPartitions, playerTransform);
+
         foreach (var cell in playerCellArray)
         {
-            Vector3 pos = Vector3.zero;
-            pos.x += cell.x * cellSize.x;
-            pos.y += cell.y * cellSize.y;
-            pos.z += cell.z * cellSize.z;
+            Vector3 pos = offset;
+            pos.x *= cellSize.x;
+            pos.y *= cellSize.y;
+            pos.z *= cellSize.z;
+
+            pos.x += (cell.x + 0.5f) * cellSize.x;
+            pos.y += (cell.y + 0.5f) * cellSize.y;
+            pos.z += (cell.z + 0.5f) * cellSize.z;
             Gizmos.DrawWireCube(pos, cellSize);
+        }
+
+        var spawnLocations = Object.FindObjectsOfType<SpawnLocation>();
+
+        foreach(var location in spawnLocations)
+        {
+            IVec3 locationCell = spawnPartitions.GetCell(location);
+            foreach(var playerCell in playerCellArray)
+            {
+                if(locationCell == playerCell)
+                {
+                    Gizmos.DrawSphere(location.transform.position, 1.0f);
+                }
+            }
         }
     }
 }
