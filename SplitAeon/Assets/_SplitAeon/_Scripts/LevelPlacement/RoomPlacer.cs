2d21f4115a3bb6ebde4d4340884ea13e5b1c706d
Lucas Petersen <ima_lucas@hotmail.com>
2021-09-29 04:29:07 +1000

    Flesh out Room Placer Editor
    The Room placer script can now Move Gameobjects that are linked by pairs automatically in the editor when one of the objects is moved. (eg. a room in the past  is moved to the right 5 units. The linked room in the future will also move right 5 units.)

--- SplitAeon/Assets/_SplitAeon/_Scripts/LevelPlacement/RoomPlacer.cs
+++ SplitAeon/Assets/_SplitAeon/_Scripts/LevelPlacement/RoomPlacer.cs
@@ -4,14 +4,68 @@ using UnityEngine;
 
 public class RoomPlacer : MonoBehaviour
 {
+    public Vector3 offset = Vector3.zero;
+    public List<RoomPair> roomPairs;
+
     [System.Serializable]
-    public struct RoomPair
+    public class RoomPair
     {
+        public string roomName = "New Room Pair";
         public GameObject pastRoom;
         public GameObject futureRoom;
+
+        public void AlignToPast(Vector3 offset)
+        {
+            if(IsFullyAssigned())
+            {
+                AlignTransforms(pastRoom.transform, futureRoom.transform, offset);
+            }
+        }
+
+        public void AlignToFuture(Vector3 offset)
+        {
+            if (IsFullyAssigned())
+            {
+                AlignTransforms(futureRoom.transform, pastRoom.transform, -offset);
+            }
+        }
+
+        void AlignTransforms(Transform origin, Transform target, Vector3 offset)
+        {
+            target.transform.position = origin.transform.position + offset;
+            target.transform.rotation = origin.transform.rotation;
+            target.transform.localScale = origin.transform.localScale;
+        }
+
+        bool IsFullyAssigned()
+        {
+            return pastRoom != null && futureRoom != null;
+        }
     }
 
-    public Vector3 offset = Vector3.zero;
+    public void AlignRoomPairToPast(RoomPair roomPair)
+    {
+        roomPair.AlignToPast(offset);
+    }
 
-    public List<RoomPair> roomPairs;
+    public void AlignRoomPairToFuture(RoomPair roomPair)
+    {
+        roomPair.AlignToFuture(offset);
+    }
+
+    public void AlignAllRoomPairsToPast()
+    {
+        foreach(RoomPair roomPair in roomPairs)
+        {
+            AlignRoomPairToPast(roomPair);
+        }
+    }
+
+    public void AlignAllRoomPairsToFuture()
+    {
+        foreach (RoomPair roomPair in roomPairs)
+        {
+            AlignRoomPairToFuture(roomPair);
+        }
+    }
 }
