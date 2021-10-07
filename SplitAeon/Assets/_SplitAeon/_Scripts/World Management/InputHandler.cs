8c5e72cd73ed09bacaddd66f82093165b72156e1
Connor <connormccaskie@gmail.com>
2021-10-07 16:40:58 +1100

    Continued implementing new input system

---
+++ SplitAeon/Assets/_SplitAeon/_Scripts/World Management/InputHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    private UserActions userActions;
+    private InputAction movementForward;
+    private InputAction movementRight;
+
+    private void OnEnable()
+    {
+        movementForward = userActions.PlayerMap.MoveForward;
+        movementForward.Enable();
+        movementRight = userActions.PlayerMap.MoveRight;
+        movementRight.Enable(); 
+
+
+    }
+}
