69d591ddb8f6ced1f3f05f5473b766ea3736dcd2
Lucas Petersen <ima_lucas@hotmail.com>
2021-10-11 16:30:13 +1100

    WIP Tracking which room the player will be in for spawning purposes.
    Rooms are Identified by finding the bounds of all the objects.
    Player exiting/entering/identifying rooms doesn't seem to function correctly. Still needs to be looked at more

---
+++ SplitAeon/Assets/_SplitAeon/_Scripts/AI/Spawning/PlayerManagement/RoomTracker.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTracker : MonoBehaviour
+{
+    [HideInInspector] public RoomBounds currentRoom;
+    RoomBounds m_previousRoom;
+    [HideInInspector] public RoomBounds previousRoom { get { return m_previousRoom; } set { m_previousRoom = value; if (value) { Debug.Log(value.name); } else { Debug.Log("null"); } } }
+
+    public int index = 0;
+    public string roomName = "";
+    public string prevName = "";
+
+    StateMachine<RoomTracker> stateMachine;
+
+    static RoomBounds[] _allRooms;
+    public static RoomBounds[] allRooms { get { return _allRooms; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _allRooms = FindObjectsOfType<RoomBounds>();
+
+        foreach(var room in _allRooms)
+        {
+            if(room.EntryContainsPoint(transform.position))
+            {
+                currentRoom = room;
+            }
+        }
+
+        if(currentRoom == null)
+        {
+            // Player did not start in a room, should find closest
+            float lowestDistance = _allRooms[0].Distance(transform.position);
+            currentRoom = _allRooms[0];
+
+            for(int  i = 1; i < _allRooms.Length; i++)
+            {
+                float dist = _allRooms[i].Distance(transform.position);
+                if(dist < lowestDistance)
+                {
+                    lowestDistance = dist;
+                    currentRoom = _allRooms[i];
+                }
+            }
+        }
+
+        previousRoom = currentRoom;
+
+        stateMachine = new StateMachine<RoomTracker>(this);
+        RoomTrackerStateBucket.SetUpStateMachine(stateMachine);
+        stateMachine.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stateMachine.Update();
+        index = stateMachine.currentIndex;
+        if(currentRoom != null)
+        {
+            roomName = currentRoom.name;
+        }
+        else
+        {
+            roomName = "null";
+        }
+        prevName = previousRoom.name;
+    }
+
+    public void SetState(int stateIndex)
+    {
+        stateMachine.ChangeState(stateIndex);
+    }
+
+    public bool FindCurrentRoomFromPrevNeighbours()
+    {
+        Vector3 position = transform.position;
+        // Try to find neighbour room of previous
+        foreach (RoomBounds neighbour in previousRoom.neighbours)
+        {
+            if (neighbour.EntryContainsPoint(position))
+            {
+                currentRoom = neighbour;
+                return true;
+            }
+        }
+
+        // did not find a relevant neighbour
+        return false;
+    }
+
+    private void OnDestroy()
+    {
+        _allRooms = null;
+    }
+}
---
+++ SplitAeon/Assets/_SplitAeon/_Scripts/AI/Spawning/PlayerManagement/RoomTrackerStateBucket.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RoomTrackerStateBucket
+{
+    public static void SetUpStateMachine(StateMachine<RoomTracker> target)
+    {
+        target.AddState(new FindStateDirection());
+        target.AddState(new InRoom());
+        target.AddState(new TryFindNeighbour());
+        target.AddState(new TryFindEnter());
+    }
+}
+
+public class FindStateDirection : IState<RoomTracker>
+{
+    void IState<RoomTracker>.Enter(RoomTracker tracker)
+    {
+
+    }
+
+    void IState<RoomTracker>.Update(RoomTracker tracker)
+    {
+        // Try to find neighbour room of previous first
+        if (tracker.FindCurrentRoomFromPrevNeighbours())
+        {
+            tracker.SetState(1);
+        }
+
+        // If we get here we did not enter another room
+        // now we should try to find the neighbour room repeatedly for a small amount of time
+        tracker.SetState(2);
+    }
+
+    void IState<RoomTracker>.Exit(RoomTracker tracker)
+    {
+
+    }
+}
+
+public class InRoom : IState<RoomTracker>
+{
+    void IState<RoomTracker>.Enter(RoomTracker tracker)
+    {
+        
+    }
+
+    void IState<RoomTracker>.Update(RoomTracker tracker)
+    {
+        Vector3 position = tracker.transform.position;
+        // check if the player has exited the current room
+        if (!tracker.currentRoom.ExitContainsPoint(position))
+        {
+            tracker.SetState(0);// Change to Find State
+        }
+    }
+
+    void IState<RoomTracker>.Exit(RoomTracker tracker)
+    {
+        tracker.previousRoom = tracker.currentRoom;
+        tracker.currentRoom = null;
+    }
+}
+
+public class TryFindNeighbour : IState<RoomTracker>
+{
+    int tryCount = 0;
+
+    void IState<RoomTracker>.Enter(RoomTracker tracker)
+    {
+        tryCount = 0;
+    }
+
+    void IState<RoomTracker>.Update(RoomTracker tracker)
+    {
+        if (tryCount > 60)
+        {
+            tracker.SetState(3);
+        }
+
+        // Try to find neighbour room of previous
+        if (tracker.FindCurrentRoomFromPrevNeighbours())
+        {
+            tracker.SetState(1);
+        }
+        tryCount++;
+    }
+
+    void IState<RoomTracker>.Exit(RoomTracker tracker)
+    {
+
+    }
+}
+
+public class TryFindEnter : IState<RoomTracker>
+{
+    RoomBounds enterRoom = null;
+
+    void IState<RoomTracker>.Enter(RoomTracker tracker)
+    {
+        enterRoom = null;
+    }
+
+    void IState<RoomTracker>.Update(RoomTracker tracker)
+    {
+        foreach(var room in RoomTracker.allRooms)
+        {
+            if(room.EntryContainsPoint(tracker.transform.position))
+            {
+                enterRoom = room;
+                tracker.SetState(1);
+            }
+        }
+    }
+
+    void IState<RoomTracker>.Exit(RoomTracker tracker)
+    {
+        tracker.currentRoom = enterRoom;
+    }
+}
