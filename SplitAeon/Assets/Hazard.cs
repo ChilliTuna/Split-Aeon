0d6c0b757e8d0ee1fbc9c432941cece3b8300963
Tom <karzacinc@gmail.com>
2021-11-05 10:27:40 +1100

    Hazards & Hit Sounds

---
+++ SplitAeon/Assets/Hazard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+
+    Health player;
+    float timer;
+
+    public float damageTick;
+    public float damageAmount;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Health>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (timer <= damageTick)
+            {
+                timer = 0;
+
+                player.Damage(damageAmount);
+            }
+        }
+    }
+
+
+
+}
