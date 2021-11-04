8fe80a9c99333afc89d6670a043dd3dc97828892
Connor <connormccaskie@gmail.com>
2021-11-04 15:08:07 +1100

    Finished silhouette improvements

--- SplitAeon/Assets/_SplitAeon/_Scripts/World Management/SilhouetteGenerator.cs
+++ SplitAeon/Assets/_SplitAeon/_Scripts/World Management/SilhouetteGenerator.cs
@@ -16,11 +16,11 @@ public class SilhouetteGenerator : MonoBehaviour
     public GameObject belcherAttackSilhouette;
     public GameObject belcherRunSilhouette;
 
+    public float fadeDuration = 2;
+
     private List<GameObject> silhouettes = new List<GameObject>();
 
     private float offsetAmount;
-
-    private CustomTimer timer = new CustomTimer();
 
     private void Start()
     {
@@ -28,12 +28,6 @@ public class SilhouetteGenerator : MonoBehaviour
         pastAiManager = gm.pastAIManager;
         futureAiManager = gm.futureAIManager;
         offsetAmount = GetComponent<Timewarp>().offsetAmount;
-        timer.Start();
-    }
-
-    private void Update()
-    {
-        timer.DoTick();
     }
 
     private void GetActiveAIAgents()
@@ -55,16 +49,33 @@ public class SilhouetteGenerator : MonoBehaviour
     public void CreateSilhouettes()
     {
         ClearSilhouettes();
-        timer.Reset();
         GetActiveAIAgents();
-        if (Globals.isInPast)
+        foreach (AIAgent agent in aiAgents)
         {
-            foreach (AIAgent agent in aiAgents)
+            if (agent.isAlive)
             {
-                if (agent.isAlive)
+                Vector3 newPos = agent.transform.position;
+                if (Globals.isInPast)
                 {
-                    Vector3 newPos = agent.transform.position;
                     newPos.y += offsetAmount;
+                }
+                else
+                {
+                    newPos.y -= offsetAmount;
+                }
+                if (agent.settings.enemyType == EnemyType.belcher)
+                {
+                    if (agent.currentState == StateIndex.attackPlayer)
+                    {
+                        silhouettes.Add(Instantiate(belcherAttackSilhouette, newPos, agent.transform.rotation));
+                    }
+                    else
+                    {
+                        silhouettes.Add(Instantiate(belcherRunSilhouette, newPos, agent.transform.rotation));
+                    }
+                }
+                else
+                {
                     if (agent.currentState == StateIndex.attackPlayer)
                     {
                         silhouettes.Add(Instantiate(cultistAttackSilhouette, newPos, agent.transform.rotation));
@@ -76,30 +87,12 @@ public class SilhouetteGenerator : MonoBehaviour
                 }
             }
         }
-        else
-        {
-            foreach (AIAgent agent in aiAgents)
-            {
-                if (agent.isAlive)
-                {
-                    Vector3 newPos = agent.transform.position;
-                    newPos.y -= offsetAmount;
-                    if (agent.currentState == StateIndex.attackPlayer)
-                    {
-                        silhouettes.Add(Instantiate(belcherAttackSilhouette, newPos, agent.transform.rotation));
-                    }
-                    else
-                    {
-                        silhouettes.Add(Instantiate(belcherAttackSilhouette, newPos, agent.transform.rotation));
-                    }
-                }
-            }
-        }
-        StartCoroutine(FadeSilhouettes());
+        StartCoroutine(FadeSilhouettes(fadeDuration));
     }
 
     public void ClearSilhouettes()
     {
+        StopCoroutine(FadeSilhouettes());
         for (int i = 0; i < silhouettes.Count; i++)
         {
             Destroy(silhouettes[i]);
@@ -107,30 +100,38 @@ public class SilhouetteGenerator : MonoBehaviour
         silhouettes.Clear();
     }
 
-    private IEnumerator FadeSilhouettes(int totalFadeFrames = 1, float fadeIntervals = 0.2f)
+    private IEnumerator FadeSilhouettes(float totalFadeTime = 2, float fadeIntervals = 0.2f)
     {
-        if (totalFadeFrames <= 0)
+        if (silhouettes.Count > 0)
         {
-            totalFadeFrames = 1;
-        }
-
-        int currentFade = 0;
-
-        float currentParticleAmount = silhouettes[0].GetComponent<VisualEffect>().GetFloat("Particle Spawn Rate");
-
-        while (totalFadeFrames < currentFade)
-        {
-            currentFade++;
-            currentParticleAmount *= currentFade / totalFadeFrames;
-
-            foreach (GameObject silhouette in silhouettes)
+            if (totalFadeTime <= 0)
             {
-                silhouette.GetComponent<VisualEffect>().SetFloat("Particle Spawn Rate", currentParticleAmount);
+                totalFadeTime = 1;
             }
 
-            yield return new WaitForSeconds(fadeIntervals);
-        }
+            float currentFade = 0;
 
-        ClearSilhouettes();
+            float currentParticleAmount = cultistAttackSilhouette.GetComponent<VisualEffect>().GetFloat("Particle Spawn Rate");
+
+            while (totalFadeTime > currentFade)
+            {
+                currentFade += fadeIntervals + Time.deltaTime;
+                currentParticleAmount -= currentParticleAmount * (currentFade / totalFadeTime);
+
+                if (silhouettes.Count == 0)
+                {
+                    StopCoroutine(FadeSilhouettes());
+                }
+
+                foreach (GameObject silhouette in silhouettes)
+                {
+                    silhouette.GetComponent<VisualEffect>().SetFloat("Particle Spawn Rate", currentParticleAmount);
+                }
+
+                yield return new WaitForSeconds(fadeIntervals);
+            }
+
+            ClearSilhouettes();
+        }
     }
 }
