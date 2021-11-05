137a38a28cd42d01fc7c11a617a76b8b6f5e7f95
Tom <karzacinc@gmail.com>
2021-11-05 15:38:16 +1100

    Its Real Weapon Wheel Hours

---
+++ SplitAeon/Assets/ButtonImageCopier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class ButtonImageCopier : MonoBehaviour
+{
+    Image thisImage;
+    public Button buttonToCopy;
+
+    private void Start()
+    {
+        thisImage = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        thisImage.enabled = buttonToCopy.IsInteractable();
+    }
+}
