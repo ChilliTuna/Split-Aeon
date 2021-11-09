0c1d1b4b3f371f136ed846268cc20f1f51dfa41b
Rugs141 <karim18marshall@gmail.com>
2021-11-09 22:48:29 +1100

    the weekend job #2
    Door blockage added in all appropriate areas
    Progress on collision in future & Past

---
+++ SplitAeon/Assets/_SplitAeon/_Scripts/Objectives/ObjectiveController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveController : MonoBehaviour
+{
+    public ObjectiveAsset[] objectiveAssets;
+
+    public void EnableAssets()
+    {
+        foreach (var asset in objectiveAssets)
+        {
+            asset.EnableAsset();
+        }
+    }
+
+    public void DisableAssets()
+    {
+        foreach (var asset in objectiveAssets)
+        {
+            asset.DisableAsset();
+        }
+    }
+}
